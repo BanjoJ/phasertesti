8d3440aa26122e6a80636864498329209b392bb5
BanjoJ <jyri.poytalaakso@hotmail.com>
2023-06-01 01:25:06 +0300

    Rework, koodia korjattu ja tasot lisätty

--- js/game.js
+++ js/game.js
@@ -17,136 +17,146 @@ var config = {
 var game = new Phaser.Game(config);
 
 var credits = 100;
+var pointsText;
+var level = 1;
+
 
 var path;
 var turrets;
 var enemies;
 
-var ENEMY_SPEED = 1/10000;
+var ENEMY_SPEED = 1 / 10000;
+var ENEMY_HP = 100;
+var ENEMY_DAMAGE_INCREASE = 25;
+
 
 var BULLET_DAMAGE = 50;
 
-var map =  [[ 0,-1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [ 0,-1, 0, 0, 0, 0, 0, 0, 0, 0],
-            [ 0,-1,-1,-1,-1,-1,-1,-1, 0, 0],
-            [ 0, 0, 0, 0, 0, 0, 0,-1, 0, 0],
-            [ 0, 0, 0, 0, 0, 0, 0,-1, 0, 0],
-            [ 0, 0, 0, 0, 0, 0, 0,-1, 0, 0],
-            [ 0, 0, 0, 0, 0, 0, 0,-1, 0, 0],
-            [ 0, 0, 0, 0, 0, 0, 0,-1, 0, 0]];
+var map = [[0, -1, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, -1, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, -1, -1, -1, -1, -1, -1, -1, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, -1, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, -1, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, -1, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, -1, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, -1, 0, 0]];
 
-function preload() {    
+function preload() {
     this.load.atlas('sprites', 'assets/spritesheet.png', 'assets/spritesheet.json');
     this.load.image('bullet', 'assets/bullet.png');
 }
 
 var Enemy = new Phaser.Class({
 
-        Extends: Phaser.GameObjects.Image,
+    Extends: Phaser.GameObjects.Image,
 
-        initialize:
+    initialize:
 
-        function Enemy (scene)
-        {
+        function Enemy(scene) {
             Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'enemy');
 
             this.follower = { t: 0, vec: new Phaser.Math.Vector2() };
             this.hp = 0;
         },
 
-        startOnPath: function ()
-        {
-            this.follower.t = 0;
-            this.hp = 100;
-            
-            path.getPoint(this.follower.t, this.follower.vec);
-            
-            this.setPosition(this.follower.vec.x, this.follower.vec.y);            
-        },
-        receiveDamage: function(damage) {
-            this.hp -= damage;           
-            
-            // if hp drops below 0 we deactivate this enemy
-            if(this.hp <= 0) {
-                this.setActive(false);
-                this.setVisible(false); 
-                credits += 5; // add 5 credits when enemy is destroyed
-                pointsText.setText('Points: ' + credits);
-            }
-        },
-        update: function (time, delta)
-        {
-            this.follower.t += ENEMY_SPEED * delta;
-            path.getPoint(this.follower.t, this.follower.vec);
-            
-            this.setPosition(this.follower.vec.x, this.follower.vec.y);
+    startOnPath: function () {
+        this.follower.t = 0;
+        this.hp = ENEMY_HP + (level - 1) * ENEMY_DAMAGE_INCREASE;
 
-            if (this.follower.t >= 1)
-            {
-                this.setActive(false);
-                this.setVisible(false);
+        path.getPoint(this.follower.t, this.follower.vec);
+
+        this.setPosition(this.follower.vec.x, this.follower.vec.y);
+    },
+    receiveDamage: function (damage) {
+        this.hp -= damage;
+
+        // if hp drops below 0 we deactivate this enemy
+        if (this.hp <= 0) {
+            this.setActive(false);
+            this.setVisible(false);
+            credits += 5; // add 5 credits when enemy is destroyed
+            pointsText.setText('Points: ' + credits);
+        }
+    },
+    update: function (time, delta) {
+        this.follower.t += ENEMY_SPEED * delta;
+        path.getPoint(this.follower.t, this.follower.vec);
+
+        this.setPosition(this.follower.vec.x, this.follower.vec.y);
+
+        if (time > this.nextEnemy) {
+            var enemy = enemies.get();
+            if (enemy) {
+                enemy.setActive(true);
+                enemy.setVisible(true);
+                enemy.startOnPath();
+
+                this.nextEnemy = time + 2000;
             }
         }
+
+        if (this.follower.t >= 1) {
+            this.setActive(false);
+            this.setVisible(false);
+        }
+    }
 
 });
 
 function getEnemy(x, y, distance) {
     var enemyUnits = enemies.getChildren();
-    for(var i = 0; i < enemyUnits.length; i++) {       
-        if(enemyUnits[i].active && Phaser.Math.Distance.Between(x, y, enemyUnits[i].x, enemyUnits[i].y) < distance)
+    for (var i = 0; i < enemyUnits.length; i++) {
+        if (enemyUnits[i].active && Phaser.Math.Distance.Between(x, y, enemyUnits[i].x, enemyUnits[i].y) < distance)
             return enemyUnits[i];
     }
     return false;
-} 
+}
 
 var Turret = new Phaser.Class({
 
-        Extends: Phaser.GameObjects.Image,
+    Extends: Phaser.GameObjects.Image,
 
-        initialize:
+    initialize:
 
-        function Turret (scene)
-        {
+        function Turret(scene) {
             Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'turret');
             this.nextTic = 0;
         },
-		place: function(i, j) {
-			if (credits > 9) {
-				this.y = i * 64 + 64/2;
-				this.x = j * 64 + 64/2;
-				map[i][j] = 1;  
-				credits -= 10;
-			}
-			else{
-				this.y = -100;
-				this.x = -100;
-			}           
-        },
-        fire: function() {
-            var enemy = getEnemy(this.x, this.y, 200);
-            if(enemy) {
-                var angle = Phaser.Math.Angle.Between(this.x, this.y, enemy.x, enemy.y);
-                addBullet(this.x, this.y, angle);
-                this.angle = (angle + Math.PI/2) * Phaser.Math.RAD_TO_DEG;
-            }
-        },
-        update: function (time, delta)
-        {
-            if(time > this.nextTic) {
-                this.fire();
-                this.nextTic = time + 1000;
-            }
+    place: function (i, j) {
+        if (credits > 9) {
+            this.y = i * 64 + 64 / 2;
+            this.x = j * 64 + 64 / 2;
+            map[i][j] = 1;
+            credits -= 10;
         }
+        else {
+            this.y = -100;
+            this.x = -100;
+        }
+    },
+    fire: function () {
+        var enemy = getEnemy(this.x, this.y, 200);
+        if (enemy) {
+            var angle = Phaser.Math.Angle.Between(this.x, this.y, enemy.x, enemy.y);
+            addBullet(this.x, this.y, angle);
+            this.angle = (angle + Math.PI / 2) * Phaser.Math.RAD_TO_DEG;
+        }
+    },
+    update: function (time, delta) {
+        if (time > this.nextTic) {
+            this.fire();
+            this.nextTic = time + 1000;
+        }
+    }
 });
-    
+
 var Bullet = new Phaser.Class({
 
-        Extends: Phaser.GameObjects.Image,
+    Extends: Phaser.GameObjects.Image,
 
-        initialize:
+    initialize:
 
-        function Bullet (scene)
-        {
+        function Bullet(scene) {
             Phaser.GameObjects.Image.call(this, scene, 0, 0, 'bullet');
 
             this.incX = 0;
@@ -156,72 +166,42 @@ var Bullet = new Phaser.Class({
             this.speed = Phaser.Math.GetSpeed(600, 1);
         },
 
-        fire: function (x, y, angle)
-        {
-            this.setActive(true);
-            this.setVisible(true);
-            //  Bullets fire from the middle of the screen to the given x/y
-            this.setPosition(x, y);
-            
+    fire: function (x, y, angle) {
+        this.setActive(true);
+        this.setVisible(true);
+        //  Bullets fire from the middle of the screen to the given x/y
+        this.setPosition(x, y);
+
         //  we don't need to rotate the bullets as they are round
         //    this.setRotation(angle);
 
-            this.dx = Math.cos(angle);
-            this.dy = Math.sin(angle);
+        this.dx = Math.cos(angle);
+        this.dy = Math.sin(angle);
 
-            this.lifespan = 1000;
-        },
+        this.lifespan = 1000;
+    },
 
-        update: function (time, delta)
-        {
-            this.lifespan -= delta;
+    update: function (time, delta) {
+        this.lifespan -= delta;
 
-            this.x += this.dx * (this.speed * delta);
-            this.y += this.dy * (this.speed * delta);
+        this.x += this.dx * (this.speed * delta);
+        this.y += this.dy * (this.speed * delta);
 
-            if (this.lifespan <= 0)
-            {
-                this.setActive(false);
-                this.setVisible(false);
-            }
+        if (this.lifespan <= 0) {
+            this.setActive(false);
+            this.setVisible(false);
         }
+    }
 
-    });
- 
-function create() {
-    var graphics = this.add.graphics();    
-    drawLines(graphics);
-    path = this.add.path(96, -32);
-    path.lineTo(96, 164);
-    path.lineTo(480, 164);
-    path.lineTo(480, 544);
-    
-    graphics.lineStyle(2, 0xffffff, 1);
-    path.draw(graphics);
-    
-    enemies = this.physics.add.group({ classType: Enemy, runChildUpdate: true });
-    
-    turrets = this.add.group({ classType: Turret, runChildUpdate: true });
-    
-    bullets = this.physics.add.group({ classType: Bullet, runChildUpdate: true });
-    
-    this.nextEnemy = 0;
-    
-    this.physics.add.overlap(enemies, bullets, damageEnemy);
-    
-    this.input.on('pointerdown', placeTurret);
+});
 
-    // Generate points text
-    pointsText = this.add.text(10, 10, 'Points: ' + credits, { fontSize: '24px', fill: '#ffffff' });
-}
-
-function damageEnemy(enemy, bullet) {  
+function damageEnemy(enemy, bullet) {
     // only if both enemy and bullet are alive
     if (enemy.active === true && bullet.active === true) {
         // we remove the bullet right away
         bullet.setActive(false);
-        bullet.setVisible(false);    
-        
+        bullet.setVisible(false);
+
         // decrease the enemy hp with BULLET_DAMAGE
         enemy.receiveDamage(BULLET_DAMAGE);
     }
@@ -229,30 +209,28 @@ function damageEnemy(enemy, bullet) {
 
 function drawLines(graphics) {
     graphics.lineStyle(1, 0x0000ff, 0.8);
-    for(var i = 0; i < 8; i++) {
+    for (var i = 0; i < 8; i++) {
         graphics.moveTo(0, i * 64);
         graphics.lineTo(640, i * 64);
     }
-    for(var j = 0; j < 10; j++) {
+    for (var j = 0; j < 10; j++) {
         graphics.moveTo(j * 64, 0);
         graphics.lineTo(j * 64, 512);
     }
     graphics.strokePath();
 }
 
-function update(time, delta) {  
+function update(time, delta) {
 
-    if (time > this.nextEnemy)
-    {
+    if (time > this.nextEnemy) {
         var enemy = enemies.get();
-        if (enemy)
-        {
+        if (enemy) {
             enemy.setActive(true);
             enemy.setVisible(true);
             enemy.startOnPath();
 
             this.nextEnemy = time + 2000;
-        }       
+        }
     }
 }
 
@@ -261,23 +239,59 @@ function canPlaceTurret(i, j) {
 }
 
 function placeTurret(pointer) {
-    var i = Math.floor(pointer.y/64);
-    var j = Math.floor(pointer.x/64);
-    if(canPlaceTurret(i, j)) {
+    var i = Math.floor(pointer.y / 64);
+    var j = Math.floor(pointer.x / 64);
+    if (canPlaceTurret(i, j)) {
         var turret = turrets.get();
-        if (turret)
-        {
+        if (turret) {
             turret.setActive(true);
             turret.setVisible(true);
             turret.place(i, j);
-        }   
+            pointsText.setText('Points: ' + credits);
+        }
     }
 }
 
 function addBullet(x, y, angle) {
     var bullet = bullets.get();
-    if (bullet)
-    {
+    if (bullet) {
         bullet.fire(x, y, angle);
     }
+}
+
+function increaseDifficulty() {
+    ENEMY_SPEED += 0.0001; // Increase enemy speed
+    BULLET_DAMAGE += 10; // Increase bullet damage
+    ENEMY_HP += ENEMY_DAMAGE_INCREASE; // Increase enemy HP for the next level
+    level++; // Increment level
+}
+
+function create() {
+    var graphics = this.add.graphics();
+    drawLines(graphics);
+    path = this.add.path(96, -32);
+    path.lineTo(96, 164);
+    path.lineTo(480, 164);
+    path.lineTo(480, 544);
+
+    graphics.lineStyle(2, 0xffffff, 1);
+    path.draw(graphics);
+
+    enemies = this.physics.add.group({ classType: Enemy, runChildUpdate: true });
+
+    turrets = this.add.group({ classType: Turret, runChildUpdate: true });
+
+    bullets = this.physics.add.group({ classType: Bullet, runChildUpdate: true });
+
+    this.nextEnemy = 0;
+
+    this.physics.add.overlap(enemies, bullets, damageEnemy);
+
+    this.input.on('pointerdown', placeTurret);
+
+    // Generate points text
+    pointsText = this.add.text(10, 10, 'Points: ' + credits, { fontSize: '24px', fill: '#ffffff' });
+    levelText = this.add.text(10, 40, 'Level: ' + level, { fontSize: '24px', fill: '#ffffff' }); // Display current level
+
+    increaseDifficulty();
 }
